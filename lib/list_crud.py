2e6df01f671b9d58254261b18768a0877063f94c
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-09-13 16:04:44 +0300

    done

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,44 @@
 def create_an_empty_list():
-    return None
+
+    return []
 
 def create_a_list():
-    return None
+    list= [12, 'Nic',4.5,8]
+    return list
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(list, element):
+   list= [12, 'Nic',4.5]
+   list.append(element)
+   return list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(list, element):
+    list= [12, 'Nic',4.5]
+    list.insert(0,element)
 
-def remove_element_from_end_of_list(l):
-    return None
+    return list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_end_of_list(list):
+    list.pop()
+    return list
 
-def retrieve_first_element_from_list(l):
-    return None
+def remove_element_from_start_of_list(list):
+    del(list[0])
+    return list
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_first_element_from_list(list):
+    if list:
+        return list[0]
+    else:
+        return None 
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_element_from_index(list, index):
+    if 0 <= index < len(list):
+        return list[index]
+    else:
+        return None
+
+def retrieve_last_element_from_list(list):
+    if list:
+        return list[-1]
+    else: 
+        return None
